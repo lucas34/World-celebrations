cc9849b6f5274cfa4f3d4b2117ee686d67514b73
Lucas Nelaupe <lucas34@users.noreply.github.com>
2018-03-07 14:23:04 +0800

    Helper class can be declare as object directly

--- common/src/main/java/packi/day/common/Constants.java
+++
@@ -1,7 +0,0 @@
-package packi.day.common;
-
-public class Constants {
-    public static final String PATH_NOTIFICATION = "/ongoingnotification";
-    public static final String PATH_DISMISS = "/ondismiss";
-    public static final String KEY_TITLE = "title";
-}
--- common/src/main/java/packi/day/common/Tools.kt
+++ common/src/main/java/packi/day/common/Tools.kt
@@ -7,54 +7,55 @@ import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.wearable.PutDataMapRequest
 import com.google.android.gms.wearable.Wearable
 
-class Keyboard {
+object Keyboard {
 
-    companion object {
+    fun hide(activity: Activity) {
+        val manager = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
 
-        fun hide(activity: Activity) {
-            val manager = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-
-            val windowToken = activity.currentFocus?.windowToken ?: return
-            manager.hideSoftInputFromWindow(windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
-        }
-
+        val windowToken = activity.currentFocus?.windowToken ?: return
+        manager.hideSoftInputFromWindow(windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
     }
+
 }
 
-class Wear {
+object Wear {
 
-    companion object {
+    fun sendNotification(context: Context, message: String) {
+        val apiClient = GoogleApiClient.Builder(context)
+                // TODO depreacted
+                .addApi(Wearable.API)
+                .build()
 
-        fun sendNotification(context: Context, message: String) {
-            val apiClient = GoogleApiClient.Builder(context)
-                    // TODO depreacted
-                    .addApi(Wearable.API)
-                    .build()
+        if (apiClient.isConnected) {
+            sendMessage(apiClient, message)
+        } else {
+            // TODO don't do blocking connect
+            apiClient.blockingConnect()
 
             if (apiClient.isConnected) {
                 sendMessage(apiClient, message)
-            } else {
-                // TODO don't do blocking connect
-                apiClient.blockingConnect()
-
-                if (apiClient.isConnected) {
-                    sendMessage(apiClient, message)
-                }
             }
         }
-
-        private fun sendMessage(googleApiClient: GoogleApiClient, message: String) {
-            if (googleApiClient.isConnected) {
-
-                val datas = PutDataMapRequest.create(Constants.PATH_NOTIFICATION)
-                datas.dataMap.putString(Constants.KEY_TITLE, message)
-                val request = datas.asPutDataRequest()
-
-                // TODO depreacted
-                Wearable.DataApi.putDataItem(googleApiClient, request)
-            }
-        }
-
     }
+
+    private fun sendMessage(googleApiClient: GoogleApiClient, message: String) {
+        if (googleApiClient.isConnected) {
+
+            val datas = PutDataMapRequest.create(Constants.PATH_NOTIFICATION)
+            datas.dataMap.putString(Constants.KEY_TITLE, message)
+            val request = datas.asPutDataRequest()
+
+            // TODO depreacted
+            Wearable.DataApi.putDataItem(googleApiClient, request)
+        }
+    }
+
+}
+
+object Constants {
+
+    const val PATH_NOTIFICATION = "/ongoingnotification"
+    const val PATH_DISMISS = "/ondismiss"
+    const val KEY_TITLE = "title"
 
 }
